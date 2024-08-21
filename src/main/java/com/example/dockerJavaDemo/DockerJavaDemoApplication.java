2e3a8f52d60bcb07fb734dca57347a3bbafad1bc
Kasun Purnima <kasunpurnima89@gmail.com>
2024-08-21 17:22:58 +0530

    Inital

---
+++ src/main/java/com/example/dockerJavaDemo/DockerJavaDemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.dockerJavaDemo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+// import com.example.log4j2sample.Log4jSampleApplication;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
+@EntityScan("com.example")
+@EnableJpaRepositories("com.example")
+/*public class DockerJavaDemoApplication {
+	//private static final Logger LOGGER = LoggerFactory.getLogger(DockerJavaDemoApplication.class);
+	  private static final Logger logger = LogManager.getLogger(DockerJavaDemoApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJavaDemoApplication.class, args);
+	}
+
+}*/
+public class DockerJavaDemoApplication implements ApplicationRunner {
+  private static final Logger logger = LogManager.getLogger(DockerJavaDemoApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerJavaDemoApplication.class, args);
+  }
+
+  @Override
+  public void run(ApplicationArguments applicationArguments) throws Exception {}
+	}
