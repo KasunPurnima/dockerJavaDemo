132b0546f291c3e3f09500bc811178593af62ee8
Kasun Purnima <kasunpurnima89@gmail.com>
2024-08-22 10:32:25 +0530

    update

--- src/main/java/com/example/dockerJavaDemo/DockerJavaDemoApplication.java
+++ src/main/java/com/example/dockerJavaDemo/DockerJavaDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -26,13 +27,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 	}
 
 }*/
-public class DockerJavaDemoApplication implements ApplicationRunner {
+public class DockerJavaDemoApplication extends SpringBootServletInitializer {
   private static final Logger logger = LogManager.getLogger(DockerJavaDemoApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(DockerJavaDemoApplication.class, args);
   }
-
-  @Override
+}
+/*  @Override
   public void run(ApplicationArguments applicationArguments) throws Exception {}
-	}
+	}*/
